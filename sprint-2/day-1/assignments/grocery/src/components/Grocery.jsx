3421043c7694c173d520e52dd0e3d3292555c251
nakul177 <96129513+nakul177@users.noreply.github.com>
2022-03-09 02:22:28 +0530

    try

--- sprint-2/day-1/assignments/grocery/src/components/Grocery.jsx
+++ sprint-2/day-1/assignments/grocery/src/components/Grocery.jsx
@@ -4,21 +4,25 @@ import { Groceryitems } from "./Groceryitems";
 
 export const Grocery = () => {
   const [list, setList] = useState([]);
-
+  
   const addList = (data) => {
+   
       const obj = {
           title : data,
           state : false,
+        
       }
+     
     setList([...list, obj]);
+   
   };
 
   return (
     <div>
       <h1>Grocery List</h1>
       <Groceryinput addlist={addList} />
-      {list.map((e) => (
-        <Groceryitems items={e} />
+      {list.map((e ) => (
+        <Groceryitems items={e}  />
       ))}
     </div>
   );
